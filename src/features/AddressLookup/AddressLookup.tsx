ea3effe42a705c78675f7c946f1eab72c3f143cd
Vitalii Shevtsov <shevtsov.barracuda@gmail.com>
2025-07-25 18:08:51 +0200

    chore(App): add styles

--- src/features/AddressLookup/AddressLookup.tsx
+++ src/features/AddressLookup/AddressLookup.tsx
@@ -1,26 +1,31 @@
-import { Button, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import AddressRow from './components/AddressRow';
 import { useAddressLookupStore } from './store';
 import { AiOutlinePlus } from 'react-icons/ai';
+
+import './AddressLookup.css';
 
 const AddressLookup: React.FC = () => {
   const { rows, addBlankRow, fetchAddress } = useAddressLookupStore();
 
   return (
-    <div>
-      <Heading size="xl">Address Lookup Feature</Heading>
-      <Text>Enter one or more IP addresses and get their country</Text>
-      <Button colorPalette="teal" variant="solid" onClick={() => addBlankRow()}>
-        <AiOutlinePlus /> Add
-      </Button>
+    <Box>
+      <Box marginBottom="0.75rem">
+        <Heading size="xl">Address Lookup Feature</Heading>
+        <Text>Enter one or more IP addresses and get their country</Text>
+        <Button colorPalette="cyan" variant="solid" onClick={() => addBlankRow()}>
+          <AiOutlinePlus /> Add
+        </Button>
+      </Box>
+
       {rows?.length ? (
-        <ul>
+        <ul className='list'>
           {rows.map((row) => (
             <AddressRow key={row.id} row={row} fetchAddress={fetchAddress} />
           ))}
         </ul>
       ) : null}
-    </div>
+    </Box>
   );
 };
 
--- src/features/AddressLookup/components/AddressRow/AddressRow.tsx
+++ src/features/AddressLookup/components/AddressRow/AddressRow.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect, useState, type FocusEventHandler } from 'react';
 import type { RecordRow } from '../../types';
-import { Field, Image, Input, Spinner, Stack } from '@chakra-ui/react';
+import { Box, Field, Image, Input, Spinner, Stack } from '@chakra-ui/react';
 import { Tooltip } from '@/components/ui/tooltip';
 import AddressClock from '../AddressClock';
 
@@ -17,6 +17,10 @@ const AddressRow: React.FC<Props> = ({ row, fetchAddress }) => {
   const handleInputBlur: FocusEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
 
+    if (!value.trim()) {
+      return;
+    }
+
     setLoading(true);
     fetchAddress(value, row.id);
   };
@@ -28,25 +32,27 @@ const AddressRow: React.FC<Props> = ({ row, fetchAddress }) => {
   }, [row]);
 
   return (
-    <li>
+    <li className="list-item">
       <Stack direction="row" alignItems={'center'}>
         <div className="list-item-marker">{row.id}</div>
 
-        <Field.Root invalid={false}>
+        <Field.Root flex={'1 1 70%'} invalid={false}>
           <Input disabled={loading} type="text" placeholder="0.0.0.0" onBlur={handleInputBlur} />
           <Field.ErrorText>This field is required</Field.ErrorText>
         </Field.Root>
 
-        {loading ? <Spinner size="sm" /> : null}
-        {!loading && row.record ? (
-          <>
-            <Tooltip content={row.record.country}>
-              <Image height="30px" src={row.record.flag.img} />
-            </Tooltip>
+        <Box flex={'1 1 130px'}>
+          {loading ? <Spinner size="md" /> : null}
+          {!loading && row.record ? (
+            <Stack direction="row" alignItems="center">
+              <Tooltip content={row.record.country}>
+                <Image height="30px" src={row.record.flag.img} />
+              </Tooltip>
 
-            <AddressClock timezone={row.record.timezone} />
-          </>
-        ) : null}
+              <AddressClock timezone={row.record.timezone} />
+            </Stack>
+          ) : null}
+        </Box>
       </Stack>
     </li>
   );
