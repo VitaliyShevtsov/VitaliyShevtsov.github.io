df49a10375f2f88e1669aa2e635c7cc5768f4bde
Vitalii Shevtsov <shevtsov.barracuda@gmail.com>
2026-04-21 11:33:52 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <div className={styles.board}>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
